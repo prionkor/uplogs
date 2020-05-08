199634e85a2953492553d2f3a4d0aac24d723049
Sisir K Adhikari <sisir@tutremix.com>
2020-05-08 19:31:35 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const Yup = require('yup');
+const queue = require('./queue');
+
+const logServer = process.env.LOG_SERVER;
+
+const schema = Yup.shape({
+    refId: Yup.string().required(),
+    level: Yup.string().required(),
+    service: Yup.string().required(),
+    datetime: Yup.date().default(function() {
+        return new Date();
+    }),
+    title: Yup.string().required(),
+    message: Yup.string().required(),
+});
+
+class Uplog{
+    constructor(option, queueProcessor){
+        this.option = option;
+        queueProcessor.process(this.process);
+    }
+
+    validate(){
+        if(!this.option.schema){
+            return new Promise((resolve, reject) => resolve(data));
+        }
+
+        return this.option.schema.validate(data);
+    }
+
+    queue(data){
+        this.validate(data).then(data => {
+            this.queueProcessor.add(data);
+        }).cache(e => {
+            // e : Validation error object
+            console.log('validation error');
+        });;
+    }
+
+    process(data, done){
+        axios.post(this.option.url, {
+            data
+        }).then(data => {
+            done();
+        }).catch(e => {
+            throw new Error(e);
+        });
+    }
+}
+
+const uplog = ((req, res, next) => {
+
+    if(!logServer){
+        throw new Error('No log server URL found. Please set env variable LOG_SERVER with full url of log server.');
+    }
+
+    const opt = {
+        url: logServer,
+        schema
+    }
+
+    req.logger = new Uplog(opt, queue());
+
+    next();
+});
+
+module.exports = uplog;
---
+++ src/queue.js
@@ -0,0 +1,22 @@
+const Queue = require('bull');
+
+const queueName = process.env.LOG_QUEUE_NAME || 'uplog';
+const redisQueueUri = process.env.LOG_REDIS_QUEUE_URI;
+const logQueueRateMax = process.env.LOG_QUEUE_RATE_MAX || 10;
+const logQueueRateDuration = process.env.LOG_QUEUE_RATE_DURATION || 5000;
+
+const queue = () => {
+
+    if(!redisQueueUri){
+        throw new Error('Log queue Redis URI (LOG_REDIS_QUEUE_URI) is not set.')
+    }
+
+    return new Queue(queueName, redisQueueUri, {
+        limiter: {
+            max: logQueueRateMax,
+            duration: logQueueRateDuration,
+        }
+    });
+}
+
+module.exports = queue;
